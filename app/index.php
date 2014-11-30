efe1673baab1553fd11988dffafd4b59283e415f
Brais Carrión <bcansias@gmail.com>
2014-11-30 22:23:53 +0100

    update index

--- app/index.php
+++ app/index.php
@@ -36,10 +36,25 @@
       <!-- Main component for a primary marketing message or call to action -->
       <div class="jumbotron">
         <h1>Plataforma de xesti&oacute;n de torneos online</h1>
-        <p>This example is a quick exercise to illustrate how the default, static and fixed to top navbar work. It includes the responsive CSS and HTML, so it also adapts to your viewport and device.</p>
-        <p>To see the difference between static and fixed top navbars, just scroll.</p>
         <p>
-          <a class="btn btn-lg btn-primary" href="https://github.com/braisNQ/PFC" target="_blank" role="button">Ver en Github &raquo;</a>
+        A idea deste proxecto xorde pola necesidade dun grupo de xente dedicada a organizar eventos e torneos
+         de ter unha ferramenta acorde aos tempos actuais que lles permita realizar o seu traballo.
+        </p>
+        <p>
+        Actualmente todo o proceso é manual, polo que é moi laborioso e complicado e pode dar lugar a erros. 
+        Por isto, cómpre ter unha aplicación que realice todo o proceso de creación dos eventos dun xeito máis 
+        sinxelo e eficaz.
+        </p>
+        <p>
+        Preténdese con este proxecto poder crear unha infraestrutura capaz de informatizar todo o proceso de creación, 
+        administración e desenvolvemento dos torneos e proporcionar os servizos necesarios para que mellore a experiencia 
+        dos usuarios durante a súa estadía na páxina web, seguindo ao detalle os acontecementos, así como xestionando os mesmos.
+        </p>
+        <p>
+        O código deste PFC estará dispoñible ao público baixo licenza GPL V3 (ou superior) no perfil de GitHub do autor cando estea rematado.
+        </p>
+        <p>
+        <a class="btn btn-lg btn-primary" href="https://github.com/braisNQ/PFC" target="_blank" role="button">Ver en GitHub &raquo;</a>
         </p>
       </div>
 
